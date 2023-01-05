fa451dc2fc6de7ebb2ad7e5ce34fcef5790a9d78
VitorMob <mobhacking100@gmail.com>
2023-01-05 13:58:54 -0300

    melhora teste unitario

--- test/elf_test.cpp
+++ test/elf_test.cpp
@@ -1,5 +1,5 @@
 #if DEBUG
-    #include <fmt/core.h>
+#include <fmt/core.h>
 #endif
 #include <gtest/gtest.h>
 
@@ -17,13 +17,13 @@ public:
 TEST_F(T_ElfParser, BigEndian)
 {
     m_parser.Parser("../test/elf_files/elf-Linux-PowerPC-bash");
-    
+
     // Elf Header e_ident
     EXPECT_EQ(0x10017c74, m_parser.mElfHeader().EntryPoint());
     EXPECT_EQ(52, m_parser.mElfHeader().HeaderSize());
-    EXPECT_EQ(31, m_parser.mElfHeader().SectionTableCount());    
-    EXPECT_EQ(40, m_parser.mElfHeader().SectionTableSize());    
-    EXPECT_EQ(32, m_parser.mElfHeader().ProgramTableSize());   
+    EXPECT_EQ(31, m_parser.mElfHeader().SectionTableCount());
+    EXPECT_EQ(40, m_parser.mElfHeader().SectionTableSize());
+    EXPECT_EQ(32, m_parser.mElfHeader().ProgramTableSize());
     EXPECT_EQ(20, m_parser.mElfHeader().Machine());
     EXPECT_EQ(52, m_parser.mElfHeader().ProgramOffset());
     EXPECT_EQ(952788, m_parser.mElfHeader().SectionOffset());
@@ -32,20 +32,26 @@ TEST_F(T_ElfParser, BigEndian)
     EXPECT_EQ(2, m_parser.mElfHeader().Type());
     EXPECT_EQ(0, m_parser.mElfHeader().Flags());
 
+    uint32_t offsets_name[] = {0,0x0b, 0x13, 0x21, 0x34, 0x3e, 0x46, 0x4e, 0x5b};
+    for (int i = 0; i < m_parser.mElfSection().SizeSectionHeader(); i++)
+    {
+        EXPECT_EQ(offsets_name[i], m_parser.mElfSection().Name(i));
+    }
+
     // Elf Program Header
-    //EXPECT_EQ(,);
+    // EXPECT_EQ(,);
 }
 
 TEST_F(T_ElfParser, LittleEndian)
 {
     m_parser.Parser("../test/elf_files/64_intel_ls");
-    
+
     // Elf Header e_ident
     EXPECT_EQ(0x404888, m_parser.mElfHeader().EntryPoint());
     EXPECT_EQ(64, m_parser.mElfHeader().HeaderSize());
-    EXPECT_EQ(28, m_parser.mElfHeader().SectionTableCount());    
-    EXPECT_EQ(64, m_parser.mElfHeader().SectionTableSize());    
-    EXPECT_EQ(56, m_parser.mElfHeader().ProgramTableSize());   
+    EXPECT_EQ(28, m_parser.mElfHeader().SectionTableCount());
+    EXPECT_EQ(64, m_parser.mElfHeader().SectionTableSize());
+    EXPECT_EQ(56, m_parser.mElfHeader().ProgramTableSize());
     EXPECT_EQ(62, m_parser.mElfHeader().Machine());
     EXPECT_EQ(64, m_parser.mElfHeader().ProgramOffset());
     EXPECT_EQ(108296, m_parser.mElfHeader().SectionOffset());
@@ -53,4 +59,10 @@ TEST_F(T_ElfParser, LittleEndian)
     EXPECT_EQ(1, m_parser.mElfHeader().Version());
     EXPECT_EQ(2, m_parser.mElfHeader().Type());
     EXPECT_EQ(0, m_parser.mElfHeader().Flags());
+
+    uint32_t offsets_name[] = {0, 11, 19, 33, 52, 62, 70, 78, 91};
+    for (int i = 0; i < m_parser.mElfSection().SizeSectionHeader(); i++)
+    {
+        EXPECT_EQ(offsets_name[i], m_parser.mElfSection().Name(i));
+    }
 }
