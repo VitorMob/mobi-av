e463d144f33b4675f6f24303eb5d8bce91d7bf2c
mob <mobhacking100@gmail.com>
2023-01-13 19:15:47 -0300

    Documenta codigo style doxygen

--- src/include/elf/elf_program.cpp
+++ src/include/elf/elf_program.cpp
@@ -4,7 +4,6 @@
 #endif
 #include <stdexcept>
 #include <endian.h>
-#include <string>
 
 #include "elf_program.hpp"
 
@@ -21,6 +20,7 @@
 #pragma GCC option("arch=native", "tune=native", "no-zero-upper")          // Enable AVX
 #endif
 
+// swap struct program header for 64-bit
 #define SwapEndian64Program(swap)                 \
         swap->p_offset = htobe64(swap->p_offset); \
         swap->p_filesz = htobe64(swap->p_filesz); \
@@ -30,6 +30,7 @@
         swap->p_align = htobe64(swap->p_align);   \
         swap->p_type = htobe32(swap->p_type);
 
+// swap struct program header for 32-bit
 #define SwapEndian32Program(swap)                 \
         swap->p_offset = htobe32(swap->p_offset); \
         swap->p_filesz = htobe32(swap->p_filesz); \
@@ -47,7 +48,11 @@ ElfProgram::~ElfProgram()
 {
 }
 
-// T(N+e_phnum)
+/**
+ * @brief  Will map the Program of our elf binary
+ * 
+ * @param p_fileElf struct info FileElf
+ */
 void ElfProgram::MapElfProgram(const struct FileElf &p_fileElf)
 {
         if (p_fileElf.buffer != nullptr)
@@ -112,41 +117,104 @@ void ElfProgram::MapElfProgram(const struct FileElf &p_fileElf)
         }
 }
 
+/**
+ * @brief  This member holds the number of bytes in the file image of
+           the segment.  It may be zero.
+ * 
+ * @param p_pos position
+ * @return const off_t 
+ */
 const off_t ElfProgram::Filesz(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_filesz : m_Program32.at(p_pos)->p_filesz;
 }
 
+/**
+ * @brief  This member holds a bit mask of flags relevant to the
+           segment:
+              PF_X   An executable segment.
+              PF_W   A writable segment.
+              PF_R   A readable segment.
+ * 
+ * @param p_pos position
+ * @return const uint32_t 
+ */
 const uint32_t ElfProgram::Flags(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_flags : m_Program32.at(p_pos)->p_flags;
 }
 
+/**
+ * @brief This member of the structure indicates what kind of
+              segment this array element describes or how to interpret
+              the array element's information.
+ * 
+ * @param p_pos position
+ * @return const uint32_t 
+ */
 const uint32_t ElfProgram::Type(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_type : m_Program32.at(p_pos)->p_type;
 }
 
+/**
+ * @brief This member holds the offset from the beginning of the file at which the first byte of the segment resides.
+ * 
+ * @param p_pos  position
+ * @return const off_t 
+ */
 const off_t ElfProgram::Offset(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_offset : m_Program32.at(p_pos)->p_offset;
 }
 
+/**
+ * @brief This member holds the value to which the segments are
+          aligned in memory and in the file.  Loadable process
+          segments must have congruent values for p_vaddr and
+          p_offset, modulo the page size.  Values of zero and one
+          mean no alignment is required.  Otherwise, p_align should
+          be a positive, integral power of two, and p_vaddr should
+          equal p_offset, modulo p_align.
+ * 
+ * @param p_pos position
+ * @return const off_t 
+ */
 const off_t ElfProgram::Align(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_align : m_Program32.at(p_pos)->p_align;
 }
 
+/**
+ * @brief This member holds the virtual address at which the first
+          byte of the segment resides in memory.
+ * 
+ * @param p_pos position
+ * @return const off_t 
+ */
 const off_t ElfProgram::Vaddr(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_vaddr : m_Program32.at(p_pos)->p_vaddr;
 }
 
+/**
+ * @brief On systems for which physical addressing is relevant, this
+          member is reserved for the segment's physical address.
+          Under BSD this member is not used and must be zero.
+ * 
+ * @param p_pos 
+ * @return const off_t 
+ */
 const off_t ElfProgram::Paddr(uint16_t p_pos)
 {
         return (m_class64) ? m_Program64.at(p_pos)->p_paddr : m_Program32.at(p_pos)->p_paddr;
 }
 
+/**
+ * @brief  Get size Program Header
+ * 
+ * @return const std::size_t 
+ */
 const std::size_t ElfProgram::SizeProgramHeader() const
 {
         return (m_class64) ? m_Program64.size() : m_Program32.size();
